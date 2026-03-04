d4e9fecec639a9133cc32143bd1112d1a03b2802
Christian Chinchilla Torres <christian.chinchilla@iMac-de-libert4dm.local>
2026-03-04 16:47:03 -0600

    Añadiendo archivos de clase 6

---
+++ Clase-6-CETAV/main.js
@@ -0,0 +1,36 @@
+console.log("Hola mundo!")
+
+// Variables
+const age = 33;
+var age2 = 10;
+
+var numberFloat = 15.60;
+console.log(numberFloat + 10);
+
+let numberwithZero = "15.60";
+console.log(numberwithZero + 10);
+
+//Numeros y operadores +, -, *, /
+
+const suma = 40 + 20 * 2; //se prioriza una operacion igual que en matematicas
+const otrasuma = suma + 74;
+console.log(otrasuma);
+
+//Strings - es un tipo de dato que representa texto
+let firstname = "Christian";
+let lastname = " Chinchilla";
+console.log(firstname + lastname);
+console.error("Esto es un Error");
+
+//Booleanos - son cosas de true or false
+true
+false
+ 
+const num = 10;
+console.log(num > 2);
+
+// && -> and o y
+console.log(num == 10 && num < 2); //el booleano le pregunta al numero que si es igual a 10 y si el numero es menor a 2, tiene que cumplir las 2.
+
+// || -> or -> o
+console.log(num == 0)
